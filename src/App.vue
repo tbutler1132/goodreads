8679fb7749cf00f16398f059c386fc8894387623
Timothy Butler <tbutler1132@gmail.com>
2021-07-23 01:56:07 -0400

    iFrame updated

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,51 @@
 <template>
-  <div class="app">
-    <div v-if="this.home" class="not-login">   
-      <nav>
-        <p v-if="this.user">{{this.user.username}}</p>
-        <p v-else>Welcome</p>
-        <i class="fa fa-home" v-on:click="this.page = 0"></i>
-        <p v-if="!this.user" v-on:click="this.home = false">Login</p>
-        <p v-else v-on:click="logoutUser">Logout</p>
-      </nav>
-      <main>
-        <div class="search-box" >
-          <input
-          class="search-bar" 
-          type="text" 
-          placeholder="search"
-          v-model="query"
-          @keypress="fetchBooks"
-          />
-        </div>
-        <div v-if="this.books && this.books.length > 0">
-          <ul>
-            <li v-for="book in books" :key="book.id">
-              <div class="book-result">
-                  <div class="book-cover">
-                    <img v-bind:src="book.image" />
-                  </div>
-                <div class="book-info">
-                  <span class="book-title">{{ book.title }}</span>
-                    <span> by {{book.author}}</span>
+  <div id="app">
+    <main>
+      <div class="search-box" >
+        <input
+        class="search-bar" 
+        type="text" 
+        placeholder="search"
+        v-model="query"
+        @keypress="fetchBooks"
+        />
+      </div>
+      <div v-if="this.books && this.books.length > 0">
+        <iFrame width="750px" height="750px" v-if="this.iFrame" v-bind:src="this.iFrame"></iFrame>
+        <ul v-if="!this.iFrame">
+          <li  v-for="book in books" :key="book.id" v-on:click="fetchReviews(book.id)">
+            <div class="book-result">
+                <div class="book-cover">
+                  <img v-bind:src="book.image" />
                 </div>
-              <p v-on:click="addBook(book.title, book.author, book.image)">+</p>
+              <div class="book-info">
+                <span class="book-title">{{ book.title }}</span>
+                <span> by {{book.author}}</span>
+                <span> {{book.rating}}</span>
               </div>
-            </li>
-          </ul>
-          <span v-if="this.page >= 1">Page: {{page}}</span>
-          <div v-if="this.books" class="page-buttons">
-            <button class="page" v-on:click="fetchNextPage" v-if="this.page >= 2" value="prev">Prev</button>
-            <button class="page" v-on:click="fetchNextPage" v-if="this.page >= 1" value="next">Next</button>
-          </div>
+            </div>
+          </li>
+        </ul>
+        <span v-if="this.page >= 1">Page: {{page}}</span>
+        <div v-if="this.books" class="page-buttons">
+          <button v-on:click="fetchPage" v-if="this.page >= 2" value="prev">Prev</button>
+          <button v-on:click="fetchPage" v-if="this.page >= 1" value="next">Next</button>
         </div>
+      </div>
         <div v-else-if="this.books && this.books.length === 0">
-          <span>No more books to display!</span>
+          <span>No Results...</span>
         </div>
         <div v-else-if="this.page > 0 && !this.books">
           <span>Loading...</span>
         </div>
         <div v-else>
-          <span v-if="!this.user">Search for a book!</span>
-          <div v-else class="user-books">
-            <ul v-if="this.user.books.length > 0">
-              <li v-for="book in this.user.books" :key="book._id">
-                <div class="book-result">
-                  <div class="book-cover">
-                    <img v-bind:src="book.image" />
-                  </div>
-                  <div class="book-info">
-                    <span class="book-title">{{ book.title }}</span>
-                    <span> by {{book.author}}</span>
-                  </div>
-                <p v-on:click="deleteBook(book._id)">-</p>
-                </div>
-              </li>
-            </ul>
-            <span v-else>Search books to add to your reading list!</span>
-          </div>
+          <span>Search for a book!</span>
         </div>
-      </main>
-    </div>
-    <div v-else class="user-form">
-      <form v-if="!this.newUser" v-on:submit="(e) => signinUser(e, 'signin')">
-        <label>Username</label>
-        <input v-model="this.username" type="text" />
-        <label>Password</label>
-        <input v-model="this.password" type="password" />
-        <button type="submit">Signin</button>
-        <p v-on:click="this.newUser = true">Not a user? Signup!</p>
-      </form>
-      <form v-else v-on:submit="(e) => signinUser(e, 'signup')">
-        <label>Username</label>
-        <input v-model="this.username" type="text" />
-        <label>Password</label>
-        <input v-model="this.password" type="password" />
-        <button id="sign-in" class="page" type="submit">Signup</button>
-        <p v-on:click="this.newUser = false">Already a user? Sign in!</p>
-      </form>
-    </div>
+    </main>
   </div>
 </template>
 
 <script>
-
-import axios from 'axios'
-
 
 export default {
   name: 'App',
@@ -101,55 +55,35 @@ export default {
       query: '',
       page: 0,
       books: false,
-      home: true,
-      username: '',
-      password: '',
-      user: false,
-      newUser: true,
-    }
-  },
-  async created () {
-    const token = localStorage.getItem('token')
-    const profile = localStorage.getItem("profile")
-    if(token){
-        const profileObj = JSON.parse(profile)
-        axios({
-        method: 'GET',
-        url: `${this.base_url}/users/${profileObj._id}`,
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      })
-      .then(data=> {
-        this.user = data.data
-      })
+      iFrame: false
     }
   },
   methods: {
     fetchBooks (e) {
+      this.iFrame = false
       const queryJoined = this.query.split(' ').join('+')
       if (e.key == "Enter"){
         this.page = 1
-        axios(`${this.base_url}/books/search?term=${queryJoined}&page=${this.page}`)
+        fetch(`${this.base_url}/books/search?term=${queryJoined}&page=${this.page}`)
+        .then(r => r.json())
         .then(data => {
-          this.setBooks(data.data)})
+          this.setBooks(data)})
         .catch(err => {
           console.log(err)
           this.page = 0
           alert("Something went wrong")})
       }
     },
-    fetchNextPage (e) {
+    fetchPage (e) {
       this.books = false
       if(e.target.value === "next"){
         this.page = this.page + 1
       }else{
         this.page = this.page - 1
       }
-        axios(`${this.base_url}/books/search?term=${this.query}&page=${this.page}`)
-        .then(data => this.setBooks(data.data))
+        fetch(`${this.base_url}/books/search?term=${this.query}&page=${this.page}`)
+        .then(r => r.json())
+        .then(data => this.setBooks(data))
         .catch(err => {
           console.log(err)
           this.page = 0
@@ -159,45 +93,13 @@ export default {
     },
     setBooks(books) {
       this.books = books
+      console.log(this.books)
     },
-    signinUser(event, type){
-      event.preventDefault()
-      const options = {
-        method: 'post',
-        url: `${this.base_url}/users/${type}`,
-        data: {
-          username: this.username,
-          password: this.password
-        }
-      }
-      axios(options)
-      .then(data => {
-        this.user = data.data.result, 
-        this.home = true
-        localStorage.setItem("token", data.data.token)
-        localStorage.setItem("profile", JSON.stringify({...data.data.result}))
-      })
-    },
-    logoutUser (){
-      localStorage.removeItem("token")
-      localStorage.removeItem("profile")
-      this.user = false
-    },
-    async addBook (title, author, image) {
-      console.log(title, author, image)
-      const user = await axios.post(`${this.base_url}/users/${this.user._id}/books`, {
-        title: title,
-        author: author,
-        image: image
-      })
-      console.log(user)
-      this.user = user.data
-    },
-    async deleteBook (bookId) {
-      const user = await axios.delete(`${this.base_url}/users/${this.user._id}/books/${bookId}`)
-      console.log(user.data)
-      this.user = user.data
-      console.log(this.user)
+    fetchReviews (id) {
+      console.log(id)
+      fetch(`${this.base_url}/books/reviews/?id=${id}`)
+      .then(r => r.json())
+      .then(data => this.iFrame = data)
     }
   }
 
@@ -210,9 +112,9 @@ export default {
 <style>
 
 body {
-background: rgb(131,58,180);
-background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(168,69,252,1) 100%);
-scroll-behavior: smooth;
+  background: rgb(131,58,180);
+  background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(168,69,252,1) 100%);
+  scroll-behavior: smooth;
 }
 
 #app {
@@ -230,7 +132,6 @@ scroll-behavior: smooth;
   margin-bottom: 30px;
   display: flex;
   justify-content: center;
-  margin-top: 4%;
 }
 .search-box .search-bar {
   display: flex;
@@ -277,10 +178,6 @@ ul {
   width: 75%
 }
 
-.book-cover {
-
-}
-
 .book-title{
   font-weight: bold;
 }
@@ -289,8 +186,9 @@ ul {
   display: flex;
   justify-content: center
 }
+  
 
-button.page {
+button {
 	color: #fff !important;
 	text-transform: uppercase;
 	text-decoration: none;
@@ -301,71 +199,5 @@ button.page {
 	border: none;
 	transition: all 0.4s ease 0s;
   cursor: pointer;
-}
-
-#sign-in{
-  width: 50%;
-  border: solid;
-  margin-left: 25%;
-  margin-right: 25%;
-}
-
-.my-button {
-    color: #333;
-    background-color: #fff;
-    border-color: #ccc;
-}
-.my-button:focus {
-    color: #333;
-    background-color: #e6e6e6;
-    border-color: #8c8c8c;
-}
-.my-button:hover {
-    color: #333;
-    background-color: #e6e6e6;
-    border-color: #adadad;
-}
-.my-button:active {
-    color: #333;
-    background-color: #e6e6e6;
-    border-color: #adadad;
-}
-
-nav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: auto;
-  width: 25%;
-  float: center;
-  transition: all .2s ease-in-out; 
-}
-
-
-nav > p {
-  cursor: pointer;
-  transition: all .2s ease-in-out; 
-}
-
-nav > p:hover{
-  transform: scale(1.5); 
-}
-
-i {
-  cursor: pointer;
-  transition: all .2s ease-in-out; 
-}
-
-i:hover{
-  transform: scale(1.8); 
-}
-
-form{
-  display: flex;
-  flex-direction: column;
-  width: 20%;
-  margin: auto;
-  justify-content: space-around;
-  height: 300px;
 }
 </style>
